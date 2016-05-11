882b92cb238f33b01807708742037a457fd12d88
PureXYZ <andydai@live.ca>
2016-05-11 17:29:03 -0400

    Added code for inserting a row

--- html-parse.py
+++ html-parse.py
@@ -16,6 +16,8 @@ field_type_txt = "TEXT"
 conn = sqlite3.connect(sqlite_out)
 c = conn.cursor()
 
+#Create table
+
 c.execute("CREATE TABLE {tn} ({idf} {idft})".format\
           (tn = main_table, idf = id_field, idft = field_type_int,))
 
@@ -24,6 +26,11 @@ c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct} DEFAULT '{df}'"\
 
 c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct} DEFAULT '{df}'"\
         .format(tn=main_table, cn=text_field, ct=field_type_txt, df="nore"))
+
+
+#   Code for inserting a row
+#c.execute("INSERT INTO main_table (id_num,chinese_char,text_field) VALUES ({id_num}, '{cc}', '{cd}')"\
+#          .format(id_num=insert_num, cc=insert_char, cd=insert_text))
 
 
 conn.commit()

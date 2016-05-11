b3fa42f81160108ca4a5cd79183c0441864ffe6d
PureXYZ <andydai@live.ca>
2016-05-11 17:12:07 -0400

    Added code to create sqlite db

--- html-parse.py
+++ html-parse.py
@@ -1,3 +1,30 @@
-input_html = open("input.html", "r")
+import sqlite3
+import os
 
-print input_html
+input_html = open("input.html", "r")
+os.remove("output.sqlite")
+
+sqlite_out = "output.sqlite"
+main_table = "main_table"
+id_field = "id_num"
+char_field = "chinese_char"
+text_field = "text_field"
+field_type_int = "INTEGER"
+field_type_txt = "TEXT" 
+
+
+conn = sqlite3.connect(sqlite_out)
+c = conn.cursor()
+
+c.execute("CREATE TABLE {tn} ({idf} {idft})".format\
+          (tn = main_table, idf = id_field, idft = field_type_int,))
+
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct} DEFAULT '{df}'"\
+        .format(tn=main_table, cn=char_field, ct=field_type_txt, df="gg"))
+
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct} DEFAULT '{df}'"\
+        .format(tn=main_table, cn=text_field, ct=field_type_txt, df="nore"))
+
+
+conn.commit()
+conn.close()

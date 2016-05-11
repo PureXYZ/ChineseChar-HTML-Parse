4bdb332de5232be60752fd3ca9d365eab7827cd4
PureXYZ <andydai@live.ca>
2016-05-11 19:26:06 -0400

    Added <TR> tag parsing code

--- html-parse.py
+++ html-parse.py
@@ -33,6 +33,14 @@ c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct} DEFAULT '{df}'"\
 #          .format(id_num=insert_num, cc=insert_char, cd=insert_text))
 
 
+def read_tr(line):
+    return line
+
+
+def strip_tags(line):
+    return line
+
+
 while True:
     line = input_html.readline()
     if not line:
@@ -45,16 +53,63 @@ while True:
 
 while True:
     line = input_html.readline()
+    
     if not line:
         print "Reached end of file."
         break
-    
-    if line[:4] == "<TR>":
-        print line
-        
-    else:
-        print "Reached end of table"
+    elif line == '</TABLE id="end">\n'
+        print "Reached end of table."
         break
+    
+        
+    start_tag_index = 0
+    tag_num = 1
+    is_open_tag = False
+
+    line = line.rstrip('\n')
+
+    for index,line_char in enumerate(line):
+        
+        if index + 4 < len(line):
+            
+            if line[index:index + 4] == "<TR>":
+                
+                if is_open_tag:
+                    print "Error! Nested tags!"
+                    break
+                
+                start_tag_index = index + 4
+                is_open_tag = True
+                
+            elif line[index:index + 5] == "</TR>":
+                
+                if not is_open_tag:
+                    print "Error! Extra close tag!"
+                    break
+                
+                if tag_num == 1:
+                    insert_num =  strip_tags(line[start_tag_index:index])
+                    tag_num += 1
+                elif tag_num == 2:
+                    insert_char = strip_tags(line[start_tag_index:index])
+                    tag_num += 1
+                elif tag_num == 3:
+                    insert_text = strip_tags(line[start_tag_index:index])
+                    tag_num += 1
+
+                is_open_tag = False
+        else:
+            break
+        
+    c.execute("INSERT INTO main_table (id_num,chinese_char,text_field) VALUES ({id_num}, '{cc}', '{cd}')"\
+              .format(id_num=insert_num, cc=insert_char, cd=insert_text))
+        
+            
+        
+
+
+
+
 
 
 
